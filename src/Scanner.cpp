06174aec3e7f3795fbdddde06aed98f026743b2a
pyroMechanical <63129694+pyroMechanical@users.noreply.github.com>
2021-06-15 00:23:34 -0700

    Added lexical scanner

---
+++ src/Scanner.cpp
@@ -0,0 +1,229 @@
+#include "Scanner.h"
+
+#include <cstring>
+
+namespace ash
+{
+	namespace util
+	{
+		inline static bool isAlpha(char c)
+		{
+			return (c >= 'a' && c <= 'z') ||
+				(c >= 'A' && c <= 'Z') ||
+				c == '_';
+		}
+
+		inline static bool isNumeric(char c)
+		{
+			return c >= '0' && c <= '9';
+		}
+
+		inline static bool isAlphaNumeric(char c)
+		{
+			return isAlpha(c) || isNumeric(c);
+		}
+	}
+
+	inline bool Scanner::isAtEnd()
+	{
+		return current == '\0';
+	}
+
+	inline char Scanner::advance()
+	{
+		current++;
+		return current[-1];
+	}
+
+	inline char Scanner::peek(int index)
+	{
+		if (index > (int)strlen(current) || isAtEnd()) return '\0';
+		return current[index];
+	}
+
+	inline bool Scanner::match(char expected)
+	{
+		if (isAtEnd()) return false;
+		if (*current != expected) return false;
+		current++;
+		return true;
+	}
+
+	Token Scanner::makeToken(TokenType type)
+	{
+		Token token;
+		token.type = type;
+		token.start = start;
+		token.length = (int)(current - start);
+		token.line = line;
+		return token;
+	}
+	
+	Token Scanner::errorToken(const char* message)
+	{
+		Token token;
+		token.type = TokenType::ERROR;
+		token.start = message;
+		token.length = (int)strlen(message);
+		token.line = line;
+		return token;
+	}
+
+	Token Scanner::identifierToken()
+	{
+		while (util::isAlphaNumeric(peek())) advance();
+		return makeToken(identifierType());
+	}
+
+	Token Scanner::numberToken()
+	{
+		while (util::isNumeric(peek())) advance();
+
+		if (peek() == '.')
+		{
+			advance();
+
+			while (util::isNumeric(peek())) advance();
+
+			return makeToken(TokenType::FLOAT);
+		}
+		return makeToken(TokenType::INT);
+	}
+
+	Token Scanner::stringToken()
+	{
+		while (peek() != '"' && !isAtEnd())
+		{
+			if (peek() == '\n') line++;
+			advance();
+		}
+
+		if (isAtEnd()) return errorToken("Unterminated string.");
+		advance();
+		return makeToken(TokenType::STRING);
+	}
+
+	Token Scanner::scanToken()
+	{
+		skipWhitespace();
+		start = current;
+		if (isAtEnd()) return makeToken(TokenType::EOF);
+
+		char c = advance();
+		if (util::isAlpha(c)) return identifierToken();
+		if (util::isNumeric(c)) return numberToken();
+
+		switch(c)
+		{
+		case '(': return makeToken(TokenType::LEFT_PAREN);
+		case ')': return makeToken(TokenType::RIGHT_PAREN);
+		case '{': return makeToken(TokenType::LEFT_BRACE);
+		case '}': return makeToken(TokenType::RIGHT_BRACE);
+		case ';': return makeToken(TokenType::SEMICOLON);
+		case ':': return makeToken(TokenType::COLON);
+		case ',': return makeToken(TokenType::COMMA);
+		case '.': return makeToken(TokenType::DOT);
+		case '-': return makeToken(TokenType::MINUS);
+		case '+': return makeToken(TokenType::PLUS);
+		case '/': return makeToken(TokenType::SLASH);
+		case '*': return makeToken(TokenType::STAR);
+		case '=': return makeToken(TokenType::EQUAL);
+		case '!': return makeToken(match('=') ? TokenType::BANG_EQUAL : TokenType::BANG);
+		case '<': return makeToken(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS);
+		case '>': return makeToken(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER);
+		case '"': return stringToken();
+		}
+
+		return errorToken("Unexpected character.");
+	}
+
+	void Scanner::skipWhitespace()
+	{
+		for (;;)
+		{
+			char c = peek();
+			switch (c)
+			{
+				case '\n':
+					line++;
+				case ' ':
+				case '\r':
+				case '\t':
+					advance();
+					break;
+				case '/':
+				{
+					int nestedComments = 0;
+					if (peek(1) == '/')
+					{
+						while (peek() != '\n' && !isAtEnd()) advance();
+					}
+					else if (peek(1) == '*')
+					{
+						nestedComments++;
+						while (nestedComments > 0 && !isAtEnd())
+						{
+							advance();
+							if (peek() == '/' && peek(1) == '*')
+								nestedComments++;
+							if (peek(2) == '*' && peek(3) == '/')
+								nestedComments--;
+						}
+					}
+					else return;
+					break;
+				}
+				default: 
+					return;
+			}
+		}
+	}
+
+	TokenType Scanner::checkKeyword(int tokenStart, int length, const char* rest, TokenType type)
+	{
+		if (current - start == tokenStart + length && memcmp(start + tokenStart, rest, length) == 0) return type;
+		return TokenType::IDENTIFIER;
+	}
+
+	TokenType Scanner::identifierType()
+	{
+		switch (start[0])
+		{
+		case 'a':
+			if (current - start > 1)
+			{
+				switch (start[1])
+				{
+				case 'n':
+					if (current - start > 2)
+					{
+						switch (start[2])
+						{
+						case 'd': return TokenType::AND;
+						case 'y': return TokenType::ANY;
+						}
+					}
+					break;
+				case 'u':
+					return checkKeyword(2, 2, "to", TokenType::AUTO);
+				}
+			}
+			break;
+		case 'd': return checkKeyword(1, 2, "ef", TokenType::DEF);
+		case 'e': return checkKeyword(1, 3, "lse", TokenType::ELSE);
+		case 'f':
+			if (current - start > 1)
+				switch(start[1])
+				{
+				case 'a': return checkKeyword(2, 3, "lse", TokenType::FALSE);
+				case 'o': return checkKeyword(2, 1, "r", TokenType::FOR);
+				}
+			break;
+		case 'i': return checkKeyword(1, 1, "f", TokenType::IF);
+		case 'n': return checkKeyword(1, 3, "ull", TokenType::NULL_);
+		case 'o': return checkKeyword(1, 1, "r", TokenType::OR);
+		case 'r': return checkKeyword(1, 5, "eturn", TokenType::RETURN);
+		case 't': return checkKeyword(1, 3, "rue", TokenType::TRUE);
+		}
+	}
+}
--- src/Scanner.h
+++ src/Scanner.h
@@ -1,25 +1,68 @@
 #pragma once
 
-enum class TokenType
-{
-	LEFT_PAREN, RIGHT_PAREN,
-	LEFT_BRACE, RIGHT_BRACE,
-	COMMA, DOT, MINUS, PLUS,
-	COLON, SEMICOLON, SLASH, STAR,
-	BANG, BANG_EQUAL, EQUAL, 
-	LESS, LESS_EQUAL,
-	GREATER, GREATER_EQUAL,
-	//Types
-	FLOAT, INT, STRING, FUNCTION,
-	AUTO, ANY, MULTI,
-	IDENTIFIER, DEF, 
-	//Literals
-	STRING_LIT, INT_LIT, FLOAT_LIT,
-	TRUE, FALSE, NULL,
-	//Keywords
-	IF, WHILE, FOR,
-	RETURN, ELSE,
-	AND, OR,
 
-	ERROR, EOF
-};
+namespace ash
+{
+	enum class TokenType
+	{
+		LEFT_PAREN, RIGHT_PAREN,
+		LEFT_BRACE, RIGHT_BRACE,
+		COMMA, DOT, MINUS, PLUS,
+		COLON, SEMICOLON, SLASH, STAR,
+		BANG, BANG_EQUAL, EQUAL,
+		LESS, LESS_EQUAL,
+		GREATER, GREATER_EQUAL,
+		//Types
+		AUTO, ANY, MULTI,
+		IDENTIFIER, DEF,
+		//Literals
+		TRUE, FALSE, NULL_,
+		FLOAT, INT, STRING,
+		//Keywords
+		IF, WHILE, FOR,
+		RETURN, ELSE,
+		AND, OR,
+
+		ERROR, EOF
+	};
+
+	struct Token
+	{
+		TokenType type;
+		const char* start;
+		int length;
+		int line;
+	};
+
+	class Scanner
+	{
+	private:
+		const char* start;
+		const char* current;
+		int line;
+
+		bool isAtEnd();
+		char advance();
+		char peek(int index = 0);
+
+		bool match(char expected);
+
+		void skipWhitespace();
+
+		Token makeToken(TokenType type);
+		Token errorToken(const char* message);
+		Token identifierToken();
+		Token numberToken();
+		Token stringToken();
+
+		Token scanToken();
+
+		TokenType checkKeyword(int start, int length, const char* rest, TokenType type);
+		TokenType identifierType();
+
+	public:
+		Scanner(const char* source)
+			:start(source), current(source), line(1) {}
+		~Scanner() = default;
+	};
+}
