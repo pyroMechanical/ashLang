29e3dcccd71ee99ec684b91789d1e2d81f0c3b49
pyroMechanical <63129694+pyroMechanical@users.noreply.github.com>
2021-07-01 09:30:27 -0700

    transfer from PC to laptop

--- src/pTrie.h
+++ src/pTrie.h
@@ -8,26 +8,38 @@ namespace ash
 {
 
 #define BITS_PER_NODE 6
-#define MAX_TRIE_DEPTH (sizeof(uint64_t) + BITS_PER_ARRAY - 1) / BITS_PER_ARRAY
+#define MAX_TRIE_DEPTH (sizeof(size_t) + BITS_PER_NODE - 1) / BITS_PER_NODE
 #define HASH_MASK (1 << BITS_PER_NODE) - 1
+#define ROOT_SIZE sizeof(size_t) % BITS_PER_NODE
 
-	namespace util
+	static size_t sparse_to_compact(const size_t bitmap, const size_t sparse)
 	{
-		static size_t sparse_to_compact(const size_t bitmap, const size_t sparse)
-		{
-			auto bitpos = (1 << sparse) - 1;
+		auto bitpos = (1 << sparse) - 1;
 
-			return __popcnt64(bitpos & bitmap);
-		}
-
-		class RefCounted
-		{
-		public:
-			std::atomic<size_t> count = 1;
-		};
-
+		return __popcnt64(bitpos & bitmap);
 	}
 
-	using namespace util;
+	enum class node_type {root_node, branch_node, leaf_node};
+
+	class node
+	{
+	public:
+		std::atomic<size_t> count = 1;
+		node_type type;
+		~node() = default;
+	public:
+		void add_ref()
+		{
+			std::atomic_fetch_add_explicit(&count, size_t{ 1 }, std::memory_order_relaxed);
+		}
+
+		void release()
+		{
+			if (std::atomic_fetch_sub_explicit(&count, size_t{ 1 }, std::memory_order_release) == 1)
+			{
+				this->~node();
+			}
+		}
+	};
 
 }
